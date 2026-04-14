a81c42d553713693da9f5580cdefe73cf92c7e2a
Patrick <patricksteveharrison@gmail.com>
2026-04-15 00:50:03 +0800

    Fix 3 Vercel Please work

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,9 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   cloudflare: false,
+  tanstackStart: {
+    server: {
+      preset: "vercel"
+    }
+  }
 });
